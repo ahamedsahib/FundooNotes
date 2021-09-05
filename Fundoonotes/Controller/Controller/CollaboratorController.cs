894e2766ac1deb16a2470b6e95644abfc9b86191
ahamedsahib <adsahib39@gmail.com>
2021-09-05 14:00:41 +0530

    [Ahamed]Add:Collaborator table and Collaborator model

---
+++ Fundoonotes/Controller/Controller/CollaboratorController.cs
@@ -0,0 +1,19 @@
+﻿using Manager.Manager;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fundoonotes.Controller.Controller
+{
+    public class CollaboratorController : ControllerBase
+    {
+        private readonly CollaboratorManager manager;
+
+        public CollaboratorController(CollaboratorManager manager)
+        {
+            this.manager = manager;
+        }
+    }
+}
